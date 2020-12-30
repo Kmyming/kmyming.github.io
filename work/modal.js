99ce39c4414c79fbaa844a044869be48d5ba1384
ming <kowmingyuan04@gmail.com>
2020-12-30 16:00:32 +0800

    internship experience part 1

--- work/modal.js
+++ work/modal.js
@@ -75,6 +75,17 @@ document.getElementById('carousel_coding1').addEventListener('click', function()
     website.style.display = 'none';
     internship.style.display = 'none';
 })
+document.getElementById('tinkertankerautolights').addEventListener('click', function(){
+    display.style.display = "flex";
+    street.style.display = 'none';
+    landscape.style.display = 'none';
+    travels.style.display = 'none';
+    architecture.style.display = 'none';
+    events.style.display = 'none';
+    microbit.style.display = 'flex';
+    website.style.display = 'none';
+    internship.style.display = 'none';
+})
 document.getElementById('carousel_coding2').addEventListener('click', function(){
     display.style.display = "flex";
     street.style.display = 'none';
